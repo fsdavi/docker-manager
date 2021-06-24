98dbdd31d272292cb9a60cb74853be04ebd06d65
Davi Freire <fsdavi.dev@gmail.com>
2021-06-23 22:07:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const { exec } = window.require('child_process');
+
+const App = () => {
+  const [shellResponse, setShellResponse] = React.useState([]);
+
+  React.useEffect(() => {
+    const runCommand = () => {
+      exec("docker images --format \"{{json .}}\"", (error, stdout, stderr) => {
+        if (error) {
+          setShellResponse(error.message);
+        }
+        if (stderr) {
+          setShellResponse(stderr);
+        }
+
+        setShellResponse(() => {
+          let array = stdout.split("\n");
+          let aux = array.filter(element => { if (element !== "") return element });
+          return [aux[0]];
+        });
+      });
+    };
+
+    runCommand();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      {shellResponse.map((response) => (
+        <p key={response}>
+          {response}
+        </p>
+      ))}
     </div>
   );
 }
